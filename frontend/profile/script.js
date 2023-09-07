989e69d2a987c0ca6d4cce10c39d5a82f4e733da
BEZYL MOPHAT OTIENO <96384227+Bezyl-Mophat-Otieno@users.noreply.github.com>
2023-09-07 13:09:13 +0300

    Added CI/CD principles on the project

--- frontend/profile/script.js
+++ frontend/profile/script.js
@@ -1,6 +1,8 @@
 const sales = document.querySelector("#sales");
+const posts = document.querySelector("#posts");
 const orders = document.querySelector("#orders");
 const rightNav = document.querySelector(".rightNav");
+const followersBtn = document.querySelector(".followersBtn");
 
 sales.addEventListener("click", () => {
   rightNav.classList.add("d-none");
@@ -8,4 +10,10 @@ sales.addEventListener("click", () => {
 
 orders.addEventListener("click", () => {
   rightNav.classList.add("d-none");
+});
+posts.addEventListener("click", () => {
+  rightNav.classList.add("d-none");
+});
+followersBtn.addEventListener("click", () => {
+  window.location.reload();
 });
